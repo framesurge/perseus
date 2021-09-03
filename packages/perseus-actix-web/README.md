f5036e756ce789812e08752b1e7e31b0c70d4c1c
arctic_hen7 <arctic_hen7@pm.me>
2021-09-03 18:34:33 +1000

    docs: 📝 added crate docs for `perseus-actix-web`

---
+++ packages/perseus-actix-web/README.md
@@ -0,0 +1,5 @@
+# Perseus Actix Web Integration
+
+This is the official [Perseus](https://github.com/arctic-hen7/perseus) integration for making serving your apps on [Actix Web](https://actix.rs) significantly easier!
+
+If you're new to Perseus, you should check out [the core package](https://github.com/arctic-hen7/perseus) first.
--- packages/perseus-actix-web/src/lib.rs
+++ packages/perseus-actix-web/src/lib.rs
@@ -1,3 +1,33 @@
+/*!
+ * Perseus is a blazingly fast frontend web development framework built in Rust with support for major rendering strategies,
+ * reactivity without a virtual DOM, and extreme customizability. It wraps the lower-level capabilities of[Sycamore](https://github.com/sycamore-rs/sycamore)
+ * and provides a NextJS-like API!
+ *
+ * - ✨ Supports static generation (serving only static resources)
+ * - ✨ Supports server-side rendering (serving dynamic resources)
+ * - ✨ Supports revalidation after time and/or with custom logic (updating rendered pages)
+ * - ✨ Supports incremental regeneration (build on demand)
+ * - ✨ Open build matrix (use any rendering strategy with anything else, mostly)
+ * - ✨ CLI harness that lets you build apps with ease and confidence
+ *
+ * This is the documentation for the Perseus Actix Web integration, but there's also [a CLI](https://arctic-hen7.github.io/perseus/cli.html),
+ * [the core package](https://crates.io/crates/perseus), and other integrations [integrations](https://arctic-hen7.github.io/perseus/serving.html)
+ * to make serving apps on other platforms easier!
+ *
+ * # Resources
+ *
+ * These docs will help you as a reference, but [the book](https://arctic-hen7.github.io/perseus/integrations/actix-web.html) should
+ * be your first port of call for learning about how to use Perseus and how it works.
+ *
+ * - [The Book](https://arctic-hen7.github.io/perseus)
+ * - [GitHub repository](https://github.com/arctic-hen7/perseus)
+ * - [Crate page](https://crates.io/crates/perseus)
+ * - [Gitter chat](https://gitter.im/perseus-framework/community)
+ * - [Discord server channel](https://discord.com/channels/820400041332179004/883168134331256892) (for Sycamore-related stuff)
+ */
+
+#![deny(missing_docs)]
+
 mod configurer;
 mod conv_req;
 pub mod errors;
