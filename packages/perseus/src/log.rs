945bd2a82ff0884df362ec303c38731d9b470ed8
arctic_hen7 <arctic_hen7@pm.me>
2021-09-12 06:33:29 +1000

    fix: 🐛 used absolute paths in `web_log!` macro

--- packages/perseus/src/log.rs
+++ packages/perseus/src/log.rs
@@ -2,8 +2,8 @@
 #[macro_export]
 macro_rules! web_log {
     ($format_str:literal $(, $data:expr)*) => {
-        web_sys::console::log_1(
-            &wasm_bindgen::JsValue::from(
+        ::web_sys::console::log_1(
+            &::wasm_bindgen::JsValue::from(
                 format!($format_str $(, $data)*)
             )
         );
