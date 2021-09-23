29344adc5cfb64b24e436587d97a5cc12182b2a5
arctic_hen7 <arctic_hen7@pm.me>
2021-09-23 18:08:46 +1000

    chore: 📝 added metadata for macro crate

--- packages/perseus-macro/src/lib.rs
+++ packages/perseus-macro/src/lib.rs
@@ -1,3 +1,31 @@
+/*!
+ * Perseus is a blazingly fast frontend web development framework built in Rust with support for major rendering strategies,
+ * reactivity without a virtual DOM, and extreme customizability. It wraps the lower-level capabilities of [Sycamore](https://github.com/sycamore-rs/sycamore)
+ * and provides a NextJS-like API!
+ *
+ * - ✨ Supports static generation (serving only static resources)
+ * - ✨ Supports server-side rendering (serving dynamic resources)
+ * - ✨ Supports revalidation after time and/or with custom logic (updating rendered pages)
+ * - ✨ Supports incremental regeneration (build on demand)
+ * - ✨ Open build matrix (use any rendering strategy with anything else, mostly)
+ * - ✨ CLI harness that lets you build apps with ease and confidence
+ *
+ * This is the documentation for the Perseus macros, but there's also [a CLI](https://arctic-hen7.github.io/perseus/cli.html),
+ * [the core package](https://crates.io/crates/perseus), and other [integrations](https://arctic-hen7.github.io/perseus)
+ * to make serving apps on other platforms easier!
+ *
+ * # Resources
+ *
+ * These docs will help you as a reference, but [the book](https://arctic-hen7.github.io/perseus) should
+ * be your first port of call for learning about how to use Perseus and how it works.
+ *
+ * - [The Book](https://arctic-hen7.github.io/perseus)
+ * - [GitHub repository](https://github.com/arctic-hen7/perseus)
+ * - [Crate page](https://crates.io/crates/perseus)
+ * - [Gitter chat](https://gitter.im/perseus-framework/community)
+ * - [Discord server channel](https://discord.com/channels/820400041332179004/883168134331256892) (for Sycamore-related stuff)
+ */
+
 mod test;
 
 use darling::FromMeta;
