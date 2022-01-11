6d9e16fe5ca515f26babdee71ad68f75cfce307c
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2022-01-11 10:06:31 +0100

    Basic HtmlShell struct

--- packages/perseus/src/html_shell.rs
+++ packages/perseus/src/html_shell.rs
@@ -2,56 +2,87 @@ use crate::page_data::PageData;
 use std::collections::HashMap;
 use std::env;
 
-/// Initializes the HTML shell by interpolating necessary scripts into it, as well as by adding the render configuration.
-pub fn prep_html_shell(
-    html_shell: String,
-    render_cfg: &HashMap<String, String>,
-    path_prefix: &str,
-) -> String {
-    // Define the script that will load the Wasm bundle (inlined to avoid unnecessary extra requests)
-    let load_script = format!(
-        r#"<script type="module">
-    import init, {{ run }} from "{path_prefix}/.perseus/bundle.js";
-    async function main() {{
-        await init("{path_prefix}/.perseus/bundle.wasm");
-        run();
-    }}
-    main();
-</script>"#,
-        path_prefix = path_prefix
-    );
-    // We inject a script that defines the render config as a global variable, which we put just before the close of the head
-    // We also inject a delimiter comment that will be used to wall off the constant document head from the interpolated document head
-    // We also inject the above script to load the Wasm bundle (avoids extra trips)
-    // We also inject a global variable to identify that we're testing if we are (picked up by app shell to trigger helper DOM events)
-    // We also inject a `<base>` tag with the base path of the app, which allows us to serve at relative paths just from an environment variable
-    let prepared = html_shell.replace(
-        "</head>",
-        // It's safe to assume that something we just deserialized will serialize again in this case
-        &format!(
-            "<script>window.__PERSEUS_RENDER_CFG = '{}';{testing_var}</script>\n{}\n<!--PERSEUS_INTERPOLATED_HEAD_BEGINS-->\n</head>",
-            serde_json::to_string(&render_cfg).unwrap(),
-            load_script,
-            testing_var=if env::var("PERSEUS_TESTING").is_ok() {
-                "window.__PERSEUS_TESTING = true;"
-            } else {
-                ""
-            }
-        ),
-    );
-    // Add in the `<base>` element at the very top so that it applies to everything in the HTML shell
-    // Otherwise any stylesheets loaded before it won't work properly
-    let prepared_with_base = prepared.replace(
-        "<head>",
-        &format!(
-            "<head>\n<base href=\"{path_prefix}\" />",
-            // We add a trailing `/` to the base URL (https://stackoverflow.com/a/26043021)
-            // Note that it's already had any pre-existing ones stripped away
-            path_prefix = format!("{}/", path_prefix)
-        ),
-    );
+const INTERPOLATED_HEAD_MARKER: &str = "<!-- PERSEUS_INTERPOLATED_HEAD_BEGINS -->";
 
-    prepared_with_base
+struct HtmlShell {
+    inner: String,
+}
+
+impl HtmlShell {
+    /// Initializes the HTML shell by interpolating necessary scripts into it, as well as by adding the render configuration.
+    fn new(shell: &str, render_cfg: &HashMap<String, String>, path_prefix: &str) -> Self {
+        let set_testing_flag = if env::var("PERSEUS_TESTING").is_ok() {
+            "window.__PERSEUS_TESTING = true;"
+        } else {
+            ""
+        };
+
+        // Here is the list of things that we are adding *right after* the beginning of the <head> tag:
+        //
+        // - Render config as a global variable.
+        //
+        // - Global variable to identify whether we are testing (picked up by app shell to trigger helper DOM events).
+        //
+        // - A `<base>` tag with the base path of the app, which allows us to serve at relative paths just from an environment variable.
+        //
+        //      This one has to be at the very top of the <head> element so that it applies to everything in the HTML shell.
+        //      Otherwise any stylesheets loaded before it won't work properly.
+        //
+        //      Also add a trailing `/` to the path_prefix (https://stackoverflow.com/a/26043021);
+        //      note that it's already had any pre-existing ones stripped away.
+        //
+        let script = format!(
+            r#"
+        <base href="{path_prefix}/" />
+        <script type="module">
+            window.__PERSEUS_RENDER_CFG = '{render_config}';
+            {set_testing_flag}
+    
+            import init, {{ run }} from "{path_prefix}/.perseus/bundle.js";
+            async function main() {{
+                await init("{path_prefix}/.perseus/bundle.wasm");
+                run();
+            }}
+            main();
+        </script>
+        "#,
+            path_prefix = path_prefix,
+            render_config = serde_json::to_string(render_cfg).unwrap(),
+            set_testing_flag = set_testing_flag,
+        );
+
+        let shell = shell.replace("<head>", &format!("<head>{}", script));
+
+        // Append a delimiter comment that will be used to wall off the constant document head from the interpolated document head.
+        let shell = shell.replace("</head>", &format!("{}</head>", INTERPOLATED_HEAD_MARKER));
+        Self { inner: shell }
+    }
+
+    fn interpolate_page_data(self, page_data: &PageData, root_id: &str) {
+        let html_with_head = self.inner.replace(
+            INTERPOLATED_HEAD_MARKER,
+            &format!("{0}{1}", INTERPOLATED_HEAD_MARKER, &page_data.head),
+        );
+
+        // Interpolate a global variable of the state so the app shell doesn't have to make any more trips
+        // The app shell will unset this after usage so it doesn't contaminate later non-initial loads
+        // Error pages (above) will set this to `error`
+        let state_var = format!("<script>window.__PERSEUS_INITIAL_STATE = `{}`;</script>", {
+            if let Some(state) = &page_data.state {
+                state
+                    // We escape any backslashes to prevent their interfering with JSON delimiters
+                    .replace(r#"\"#, r#"\\"#)
+                    // We escape any backticks, which would interfere with JS's raw strings system
+                    .replace(r#"`"#, r#"\`"#)
+                    // We escape any interpolations into JS's raw string system
+                    .replace(r#"${"#, r#"\${"#)
+            } else {
+                "None".to_string()
+            }
+        });
+
+        let html_with_state = html_with_head.replace("</head>", &format!("{}\n</head>", state_var));
+    }
 }
 
 /// Interpolates content, metadata, and state into the HTML shell, ready to be sent to the user for initial loads. This should be passed
@@ -159,4 +190,30 @@ pub fn interpolate_locale_redirection_fallback(
     // Now interpolate that HTML into the HTML shell
     html.replace(&html_to_replace_double, &html_replacement)
         .replace(&html_to_replace_single, &html_replacement)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::prep_html_shell;
+
+    const SHELL: &str = r#"
+<html>
+    <head>
+        <title>Shell</title>
+    </head>
+    <body>
+        <p>Content</p>
+    </body>
+</html>
+"#;
+
+    #[test]
+    fn some_example() {
+        let expected = "\n<html>\n    <head>\n<base href=\"prefix/\" />\n        <title>Shell</title>\n    <script>window.__PERSEUS_RENDER_CFG = '{}';</script>\n<script type=\"module\">\n    import init, { run } from \"prefix/.perseus/bundle.js\";\n    async function main() {\n        await init(\"prefix/.perseus/bundle.wasm\");\n        run();\n    }\n    main();\n</script>\n<!--PERSEUS_INTERPOLATED_HEAD_BEGINS-->\n</head>\n    <body>\n        <p>Content</p>\n    </body>\n</html>\n";
+        let result = prep_html_shell(String::from(SHELL), &HashMap::new(), "prefix");
+
+        assert_eq!(result, expected);
+    }
 }
