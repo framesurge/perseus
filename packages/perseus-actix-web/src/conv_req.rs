b2456dca319afd54e47bf4d8d42a63af55c03f4d
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2022-01-12 12:33:51 +0100

    Clean up HttpRequest builder logic in conv_req.rs
    Removed some unnecessary comments and rewrote repeated reassignments to
    method chaining.

--- packages/perseus-actix-web/src/conv_req.rs
+++ packages/perseus-actix-web/src/conv_req.rs
@@ -4,19 +4,15 @@ use perseus::{HttpRequest, Request};
 /// Converts an Actix Web request into an `http::request`.
 pub fn convert_req(raw: &actix_web::HttpRequest) -> Result<Request, Error> {
     let mut builder = HttpRequest::builder();
-    // Add headers one by one
+
     for (name, val) in raw.headers() {
-        // Each method call consumes and returns `self`, so we re-self-assign
         builder = builder.header(name, val);
     }
-    // The URI to which the request was sent
-    builder = builder.uri(raw.uri());
-    // The method (e.g. GET, POST, etc.)
-    builder = builder.method(raw.method());
-    // The HTTP version used
-    builder = builder.version(raw.version());
 
     builder
+        .uri(raw.uri())
+        .method(raw.method())
+        .version(raw.version())
         // We always use an empty body because, in a Perseus request, only the URI matters
         // Any custom data should therefore be sent in headers (if you're doing that, consider a dedicated API)
         .body(())
