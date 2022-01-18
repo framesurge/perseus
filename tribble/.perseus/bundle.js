29b19388c4686b5efcb0bad2ba20d98b3a2c3690
arctic-hen7 <arctic-hen7@users.noreply.github.com>
2022-01-18 20:55:57 +0000

    deploy: e12d15c2a48962b55cb9126ce818436f6b78da6d

--- tribble/.perseus/bundle.js
+++ tribble/.perseus/bundle.js
@@ -755,16 +755,16 @@ imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
 imports.wbg.__wbindgen_throw = function(arg0, arg1) {
     throw new Error(getStringFromWasm0(arg0, arg1));
 };
-imports.wbg.__wbindgen_closure_wrapper2963 = function(arg0, arg1, arg2) {
-    var ret = makeMutClosure(arg0, arg1, 594, __wbg_adapter_22);
+imports.wbg.__wbindgen_closure_wrapper2994 = function(arg0, arg1, arg2) {
+    var ret = makeMutClosure(arg0, arg1, 624, __wbg_adapter_22);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper3064 = function(arg0, arg1, arg2) {
-    var ret = makeMutClosure(arg0, arg1, 607, __wbg_adapter_25);
+imports.wbg.__wbindgen_closure_wrapper3095 = function(arg0, arg1, arg2) {
+    var ret = makeMutClosure(arg0, arg1, 637, __wbg_adapter_25);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper3208 = function(arg0, arg1, arg2) {
-    var ret = makeClosure(arg0, arg1, 636, __wbg_adapter_28);
+imports.wbg.__wbindgen_closure_wrapper3239 = function(arg0, arg1, arg2) {
+    var ret = makeClosure(arg0, arg1, 666, __wbg_adapter_28);
     return addHeapObject(ret);
 };
 
