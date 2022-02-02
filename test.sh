6f979eb4eec85c2e158524de3b730ccc251ff2fb
arctic_hen7 <arctic_hen7@pm.me>
2022-02-03 08:11:46 +1100

    fix: fixed up tests
    These won't run yet because several still need to be written.

--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # This script tests the given example with Bonnie
-test=$1
-headless=$2
+category=$1
+example=$2
+headless=$3
 
 # Get the path to the server executable (last line of output when we use `--no-run`)
-exec=$(bonnie dev test $test test --no-run | tail -n 1)
+exec=$(bonnie dev example $category $example test --no-run | tail -n 1)
 # Now move into the correct execution context
-cd tests/$test/.perseus/server
+cd examples/$category/$example/.perseus/server
 # And run the server itself in the background (making sure to pass through that we're testing)
 PERSEUS_TESTING=true $exec &
 
