60791d7520324b38971bfaf6c8d1b475ce47499e
arctic_hen7 <arctic_hen7@pm.me>
2022-02-14 19:23:41 +1100

    ci: made tests run all integrations

--- test.sh
+++ test.sh
@@ -3,10 +3,11 @@
 # This script tests the given example with Bonnie
 category=$1
 example=$2
-headless=$3
+integration=$3 # This allows us to run all the tests with every integration
+headless=$4
 
 # Get the path to the server executable (last line of output when we use `--no-run`)
-exec=$(bonnie dev example $category $example test --no-run | tail -n 1)
+exec=$(bonnie dev example $category $example test --no-run --integration $integration | tail -n 1)
 # Now move into the correct execution context
 cd examples/$category/$example/.perseus/server
 # And run the server itself in the background (making sure to pass through that we're testing)
