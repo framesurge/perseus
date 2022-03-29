6a05c6377d5300a47edd75c09bcfaf867e764b7f
arctic_hen7 <arctic_hen7@pm.me>
2022-03-29 19:59:32 +1100

    fix: made i18n example use the right locales function
    I made the mistake I explicitly documented about how to not make. Once
    again, I can *occasionally* convince myself that I have any clue of what
    I'm doing. Now is not one of those moments.

--- examples/core/i18n/src/lib.rs
+++ examples/core/i18n/src/lib.rs
@@ -8,6 +8,7 @@ pub fn main<G: Html>() -> PerseusApp<G> {
     PerseusApp::new()
         .template(crate::templates::index::get_template)
         .template(crate::templates::about::get_template)
+        .template(crate::templates::post::get_template)
         .error_pages(crate::error_pages::get_error_pages)
-        .locales("en-US", &["fr-FR", "es_ES"])
+        .locales_and_translations_manager("en-US", &["fr-FR", "es-ES"])
 }
