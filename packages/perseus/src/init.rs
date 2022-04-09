fce0db8b6643ca6723328f11d86cf662e88afacf
arctic_hen7 <arctic_hen7@pm.me>
2022-04-09 15:15:03 +1000

    fix: added utf-8 encoding to default html shell

--- packages/perseus/src/init.rs
+++ packages/perseus/src/init.rs
@@ -22,6 +22,7 @@ use sycamore::{
 static DFLT_INDEX_VIEW: &str = r#"<!DOCTYPE html>
 <html>
     <head>
+        <meta charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
