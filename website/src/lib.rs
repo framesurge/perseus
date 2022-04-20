6077181c11af6c6812322226511bf0c20b2eb188
arctic_hen7 <arctic_hen7@pm.me>
2022-04-20 19:48:09 +1000

    chore(website): updated website to v0.3.5
    Also switched away from `define_app!`.

--- website/src/lib.rs
+++ website/src/lib.rs
@@ -2,20 +2,36 @@ mod components;
 mod error_pages;
 mod templates;
 
-use perseus::{define_app, Plugins};
+use perseus::{Html, PerseusApp, PerseusRoot, Plugins};
 use perseus_size_opt::{perseus_size_opt, SizeOpts};
 
-define_app! {
-    templates: [
-        templates::index::get_template::<G>(),
-        templates::comparisons::get_template::<G>(),
-        templates::docs::get_template::<G>(),
-        templates::plugins::get_template::<G>()
-    ],
-    error_pages: error_pages::get_error_pages(),
-    locales: {
-        default: "en-US",
-        other: []
-    },
-    plugins: Plugins::new().plugin(perseus_size_opt, SizeOpts::default())
+#[perseus::main]
+pub fn main<G: Html>() -> PerseusApp<G> {
+    PerseusApp::new()
+        .template(templates::index::get_template)
+        .template(templates::comparisons::get_template)
+        .template(templates::docs::get_template)
+        .template(templates::plugins::get_template)
+        .error_pages(error_pages::get_error_pages)
+        .locales_and_translations_manager("en-US", &[])
+        .plugins(Plugins::new().plugin(
+            perseus_size_opt,
+            // Because we're using Rust 2018, we can take advantage of more aggressive size optimizations on `fluent_bundle`
+            SizeOpts::default_2018(),
+        ))
+        .index_view(|| {
+            sycamore::view! {
+                html(class = "light") {
+                    head {
+                        meta(charset = "UTF-8")
+                        meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+                        link(rel = "stylesheet", href = ".perseus/static/tailwind.css")
+                        link(rel = "stylesheet", href = ".perseus/static/styles/style.css")
+                    }
+                    body(class = "bg-white dark:bg-navy") {
+                        PerseusRoot()
+                    }
+                }
+            }
+        })
 }
