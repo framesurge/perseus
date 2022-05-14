8888935c691b9cd3d1f168134dfaa3d37ad11ded
arctic_hen7 <arctic_hen7@pm.me>
2022-05-14 20:13:59 +1000

    test: added a new example and tests for the index view

---
+++ examples/core/index_view/README.md
@@ -0,0 +1,3 @@
+# Index View Example
+
+This example showcases Perseus' ability to create a custom wrapper for the entire app using Sycamore code, rather than HTML.
---
+++ examples/core/index_view/tests/main.rs
@@ -0,0 +1,37 @@
+use fantoccini::{Client, Locator};
+use perseus::wait_for_checkpoint;
+
+#[perseus::test]
+async fn main(c: &mut Client) -> Result<(), fantoccini::error::CmdError> {
+    c.goto("http://localhost:8080").await?;
+    wait_for_checkpoint!("begin", 0, c);
+    let url = c.current_url().await?;
+    assert!(url.as_ref().starts_with("http://localhost:8080"));
+
+    // The greeting was passed through using build state
+    wait_for_checkpoint!("initial_state_present", 0, c);
+    wait_for_checkpoint!("page_visible", 0, c);
+    let greeting = c.find(Locator::Css("p")).await?.text().await?;
+    assert_eq!(greeting, "Hello World!");
+    // Check the footer, the symbol of the index view having worked
+    let footer = c.find(Locator::Css("footer")).await?.text().await?;
+    assert_eq!(footer, "This is a footer!");
+
+    // Go to `/about`
+    c.find(Locator::Id("about-link")).await?.click().await?;
+    let url = c.current_url().await?;
+    assert!(url.as_ref().starts_with("http://localhost:8080/about"));
+    wait_for_checkpoint!("initial_state_not_present", 0, c);
+    wait_for_checkpoint!("page_visible", 1, c);
+    // Make sure the hardcoded text there exists
+    let text = c.find(Locator::Css("p")).await?.text().await?;
+    assert_eq!(text, "About.");
+    // Check the footer, the symbol of the index view having worked
+    let footer = c.find(Locator::Css("footer")).await?.text().await?;
+    assert_eq!(footer, "This is a footer!");
+    // Make sure we get initial state if we refresh
+    c.refresh().await?;
+    wait_for_checkpoint!("initial_state_present", 0, c);
+
+    Ok(())
+}
