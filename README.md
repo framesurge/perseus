a16315c41c1fbc5e2b81950a0c77d9386477fb44
arctic-hen7 <arctic.hen@pm.me>
2022-11-27 08:20:09 +1100

    chore: updated `tiny` example
    It had been excluded from linting during some of the latest changes. Now
    it's back, and it's even tinier! Readme also updated.

--- README.md
+++ README.md
@@ -28,13 +28,15 @@ use sycamore::prelude::*;
 
 #[perseus::main(perseus_warp::dflt_server)]
 pub fn main<G: Html>() -> PerseusApp<G> {
-    PerseusApp::new().template(|| {
-        Template::new("index").template(|cx, _| {
-            view! { cx,
-                p { "Hello World!" }
-            }
-        })
-    })
+    PerseusApp::new()
+        .template(
+            Template::new("index")
+                .template(|cx| {
+                    view! { cx,
+                        p { "Hello World!" }
+                    }
+                })
+            )
 }
 ```
 
--- examples/comprehensive/tiny/src/main.rs
+++ examples/comprehensive/tiny/src/main.rs
@@ -4,19 +4,16 @@ use sycamore::prelude::*;
 #[perseus::main(perseus_warp::dflt_server)]
 pub fn main<G: Html>() -> PerseusApp<G> {
     PerseusApp::new()
-        .template(|| {
-            Template::new("index").template(|cx, _| {
-                view! { cx,
-                    p { "Hello World!" }
-                }
-            })
-        })
-        .error_pages(|| ErrorPages::new(
+        .template(Template::new("index").template(|cx| {
+            view! { cx,
+                p { "Hello World!" }
+            }
+        }))
+        .error_pages(ErrorPages::new(
             |cx, url, status, err, _| view! { cx,
                 p { (format!("An error with HTTP code {} occurred at '{}': '{}'.", status, url, err)) }
             },
             |cx, _, _, _, _| view! { cx,
                 title { "Error" }
-            })
-        )
+            }))
 }
