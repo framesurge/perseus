9aaa6acaae2282f4637d25ba1c5f8a76d8f36290
arctic-hen7 <arctic.hen@pm.me>
2023-01-17 07:08:18 +1030

    test(cli): added tests for `new`, `init`, and `--help`

---
+++ packages/perseus-cli/tests/help.rs
@@ -0,0 +1,19 @@
+use assert_cmd::prelude::*;
+use assert_fs::TempDir;
+use predicates::prelude::*;
+use std::process::Command;
+
+/// Makes sure `perseus --help` works (if this fails, it acts as the canary of a
+/// totally broken CLI!).
+#[test]
+fn help_works() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = TempDir::new()?;
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path()) // In dev, the CLI can be made to run anywhere!
+        .arg("--help");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("perseus [OPTIONS] <SUBCOMMAND>"));
+
+    Ok(())
+}
--- packages/perseus-cli/tests/new.rs
+++ packages/perseus-cli/tests/new.rs
@@ -1,19 +1,84 @@
 use assert_cmd::prelude::*;
-use assert_fs::TempDir;
+use assert_fs::{prelude::*, TempDir};
 use predicates::prelude::*;
 use std::process::Command;
 
-/// Makes sure `perseus new` successfully generates the hardcoded example.
-// TODO
+/// Makes sure `perseus new` successfully generates the hardcoded example with
+/// the right version of Perseus.
 #[test]
-fn default() -> Result<(), Box<dyn std::error::Error>> {
+fn new_creates_files() -> Result<(), Box<dyn std::error::Error>> {
     let dir = TempDir::new()?;
     let mut cmd = Command::cargo_bin("perseus")?;
     cmd.env("TEST_EXAMPLE", dir.path()) // In dev, the CLI can be made to run anywhere!
-        .arg("--help");
+        .arg("new")
+        .arg("my-app");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("perseus [OPTIONS] <SUBCOMMAND>"));
+        .stdout(predicate::str::contains("Your new app has been created!"));
+
+    // Make sure `Cargo.toml` has the right Perseus version
+    dir.child("my-app/Cargo.toml")
+        .assert(predicate::str::contains(env!("CARGO_PKG_VERSION")));
+    // Assert that the other files exist
+    dir.child("my-app/.gitignore")
+        .assert(predicate::path::exists());
+    dir.child("my-app/src/main.rs")
+        .assert(predicate::path::exists());
+    dir.child("my-app/src/templates/mod.rs")
+        .assert(predicate::path::exists());
+    dir.child("my-app/src/templates/index.rs")
+        .assert(predicate::path::exists());
+
+    Ok(())
+}
+
+/// Makes sure `perseus init` successfully generates the hardcoded example with
+/// the right version of Perseus.
+///
+/// Init uses the same code as `new`, so there's no point in testing whether or
+/// not it can be properly served.
+#[test]
+fn init_creates_files() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = TempDir::new()?;
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path()) // In dev, the CLI can be made to run anywhere!
+        .arg("init")
+        .arg("my-app");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Your new app has been created!"));
+
+    // Make sure `Cargo.toml` has the right Perseus version
+    dir.child("Cargo.toml")
+        .assert(predicate::str::contains(env!("CARGO_PKG_VERSION")));
+    // Assert that the other files exist
+    dir.child(".gitignore").assert(predicate::path::exists());
+    dir.child("src/main.rs").assert(predicate::path::exists());
+    dir.child("src/templates/mod.rs")
+        .assert(predicate::path::exists());
+    dir.child("src/templates/index.rs")
+        .assert(predicate::path::exists());
+
+    Ok(())
+}
+
+/// Makes sure the output of `perseus new` can be successfully served.
+#[test]
+#[ignore = "long-running"]
+fn new_can_be_served() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = TempDir::new()?;
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path()).arg("new").arg("my-app");
+    cmd.assert().success();
+
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path().join("my-app"))
+        .arg("serve")
+        // We don't want to instantiate a full server
+        .arg("--no-run");
+    cmd.assert().success().stdout(predicate::str::contains(
+        "Not running server because `--no-run` was provided.",
+    ));
 
     Ok(())
 }
