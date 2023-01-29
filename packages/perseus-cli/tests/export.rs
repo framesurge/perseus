ec7610d96000abd5b8d1105f91295e4b7c840a9e
arctic-hen7 <arctic.hen@pm.me>
2023-01-30 09:37:31 +1100

    test: added tests for `perseus export -s`

--- packages/perseus-cli/tests/export.rs
+++ packages/perseus-cli/tests/export.rs
@@ -1,3 +1,4 @@
+use crate::utils::test_serve;
 use assert_cmd::prelude::*;
 use assert_fs::{
     prelude::{PathAssert, PathChild},
@@ -19,7 +20,7 @@ fn export_produces_artifacts() -> Result<(), Box<dyn std::error::Error>> {
         .success()
         .stdout(predicate::str::contains("Your new app has been created!"));
 
-    // Build the app
+    // Export the app
     let mut cmd = Command::cargo_bin("perseus")?;
     cmd.env("TEST_EXAMPLE", dir.path()).arg("export");
     cmd.assert().success();
@@ -35,6 +36,40 @@ fn export_produces_artifacts() -> Result<(), Box<dyn std::error::Error>> {
         .assert(predicate::str::starts_with(
             r#"{"state":null,"head":"<title>Welcome to Perseus!</title>"}"#,
         ));
+
+    Ok(())
+}
+
+/// Makes sure that `perseus export -sw` serves the app.
+#[test]
+fn export_serve_serves() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = TempDir::new()?;
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path()) // In dev, the CLI can be made to run anywhere!
+        .arg("init")
+        .arg("my-app");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Your new app has been created!"));
+
+    // Export the app first
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path()).arg("export");
+    cmd.assert().success();
+
+    // And now serve it from the exported files
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path()).arg("export").arg("-s");
+    test_serve(&mut cmd, "http://localhost:8080")?;
+
+    // Try serving on a different port
+    let mut cmd = Command::cargo_bin("perseus")?;
+    cmd.env("TEST_EXAMPLE", dir.path())
+        .arg("export")
+        .arg("-s")
+        .arg("--port")
+        .arg("8000");
+    test_serve(&mut cmd, "http://localhost:8000")?;
 
     Ok(())
 }
